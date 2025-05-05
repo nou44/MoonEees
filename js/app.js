26da21d20218ba3994732df3dfab4700df417fdc
nou44 <draouiy22@gmail.com>
2025-05-05 10:03:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -373,7 +373,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 footer.classList.remove('active'); // إعادة الأنيميشن عند مغادرة القسم
             }
         });
-  });
     }, { threshold: 0.5 });
     
     observer.observe(footer);
